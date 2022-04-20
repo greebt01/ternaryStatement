823d4025d84a3c7ece4b0cf9fe1cd6c624cbf377
Birk <rgeebt01@yahoo.com>
2022-04-20 12:03:36 -0400

    replaced if with enhanced switch

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ Replaced if statement with ternary statement and added user input.
 # Update2
 Changed the topic to be about access role and used if statement
 
+# Update3
+Replaced if statement with enhanced switch.
+
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,10 @@ public class Main {
         System.out.print("What is your role?");
         Scanner scanner=new Scanner(System.in);
         String role= scanner.nextLine();
-        if (role.equals("admin")) {
-            System.out.println("You're an admin");
-        } else if (role.equals("moderator")) {
-            System.out.println("You're a moderator");
-        } else {
-            System.out.println("You're a guest");
+        switch (role) {
+            case "admin" -> System.out.println("You're an admin");
+            case "moderator" -> System.out.println("You're a moderator");
+            default -> System.out.println("You're a guest");
         }
     }
 }
